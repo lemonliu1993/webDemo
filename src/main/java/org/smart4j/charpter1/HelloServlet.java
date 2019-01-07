abb0be89494baa1a32c5bc0195a29bba2c367947
lemonliu1993 <2305247091@qq.com>
2019-01-07 23:23:09 +0800

    init project

---
+++ src/main/java/org/smart4j/charpter1/HelloServlet.java
@@ -0,0 +1,29 @@
+package org.smart4j.charpter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by lemoon on 19/1/5 下午9:49
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/jsp/hello.jsp").forward(req,resp);
+    }
+
+
+
+}
